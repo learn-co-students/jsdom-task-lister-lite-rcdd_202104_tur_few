8a04e4d72b60f4732fed489f599bde4886ab8e77
mustafaguldag <guldag.mustafaa@gmail.com>
2021-04-26 19:31:39 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  
+  
+  document.getElementById("newButton").addEventListener("click", function(e) {
+    
+    e.preventDefault();
+    
+    //let userInput = document.getElementById("new-task-description");
+    let listButton = document.createElement("button");
+    listButton.innerText = "X"
+    listButton.style.marginLeft = "5px"
+    let listUl = document.createElement("li");
+    listUl.innerHTML = (document.getElementById("new-task-description").value);
+    
+    let list1 = document.getElementById("tasks");
+    listUl.appendChild(listButton);
+    list1.appendChild(listUl);
+    list1.innerText;
+  
+
+    listButton.addEventListener("click", function() {
+      
+      
+      listUl.remove();
+    }
+    )
+
+    
+    
+    
+    
+  }
+  )
+
+
+
+
+
+}
+);
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+  
+  
+//   document.getElementById("newButton").addEventListener("click", function(e) {
+    
+//     e.preventDefault();
+    
+//     //let userInput = document.getElementById("new-task-description");
+//     let listButton = document.createElement("button");
+//     listButton.innerText = "x"
+//     listButton.style.marginLeft = "5px"
+//     let listUl = document.createElement("li");
+//     listUl.innerHTML = (document.getElementById("new-task-description").value);
+    
+//     let list1 = document.getElementById("tasks");
+//     listUl.appendChild(listButton);
+//     list1.appendChild(listUl);
+//     return list1.innerText;
+//   }
+  
+//   )
+//   let listButton = document.createElement("button");
+//   listButton.addEventListener("click", function(i) {
+//     i.preventDefault();
+//     listUl.remove();
+    
+//     }
+
+//   }
+//   )
+
+  
+  
+  
+  
+// });
