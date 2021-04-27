0f4e9829cdd084779e183ec83dd63232fdc7584b
Hafise Nur Bacaksız <hafisenur.bacaksiz@gmail.com>
2021-04-27 11:52:27 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskForm = document.getElementById("create-task-form");
+
+  taskForm.addEventListener('submit', function (e){
+   e.preventDefault()
+   let inputTask = document.getElementById("new-task-description").value;
+
+   let li = document.createElement("li");
+   let text = document.createTextNode(inputTask);
+
+   li.appendChild(text);
+   tasks.appendChild(li);
+
+  })
+
 });
