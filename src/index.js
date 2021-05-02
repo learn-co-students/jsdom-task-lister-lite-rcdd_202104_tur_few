502f4828a923cf3b24ef523a87797c827c00de47
DKay-code <derya.kaygisiz@outlook.com>
2021-05-02 14:39:22 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const submitButton = document.querySelector('#create-task-form');
+  const loc = document.querySelector('#tasks');
+  submitButton.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#new-task-description');
+    const tasksList = document.createElement('li');
+    tasksList.innerHTML=input.value;
+    loc.appendChild(tasksList);
+  });
+//   loc.addEventListener("click", (e) => {
+    
+// })
+})
