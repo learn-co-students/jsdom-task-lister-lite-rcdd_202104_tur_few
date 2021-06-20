c432b975cc6df5c1c498af5bc7d8f161e76fd8dc
Buse Şentürk <buse.senturk@outlook.com>
2021-06-20 16:42:21 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /*let newtask = document.querySelector("#new-task-description").value;
+  let tasks = document.querySelector("#tasks").value;
+let tasks = newtask;*/
+const newTaskForm = document.getElementById("create-task-form");
+ const newTaskDescription = document.getElementById("new-task-description");
+ const newTaskPriority = document.getElementById("new-task-priority");
+
+ const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask);
   // your code here
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
